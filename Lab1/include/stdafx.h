6fb100ab5cd287a20431c1f7d0033eb0c44f0a5c
GeorgeChernyshov <ridiculousman@yandex.ru>
2017-09-05 00:10:56 +0300

    Add files via upload

--- Lab1/include/stdafx.h
+++ Lab1/include/stdafx.h
@@ -1,8 +1,19 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
 #pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
 #include <math.h>
 #include <locale.h>
 #include <iostream>
 #include <limits>
+#include <string>
 
 using namespace std;
 #define stop __asm nop
