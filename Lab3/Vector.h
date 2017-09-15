90d5ee9cb1384f90320a57b4dab8d97ba7d85eb9
GeorgeChernyshov <ridiculousman@yandex.ru>
2017-09-15 13:31:54 +0400

    Update Vector.h

--- Lab3/Vector.h
+++ Lab3/Vector.h
@@ -20,6 +20,7 @@ public:
 	Vector& operator+= (const Vector& v);
 	double operator!();
 	bool operator> (const Vector& v);
+	bool operator< (const Vector& v);
 	bool operator== (const Vector& v);
 	friend const Vector operator* (const Vector& v, const double d);
 	friend const Vector operator* (const double d, const Vector& v);
